c49249a274d64262b600683e72ca7d583fb864e8
dimitrovo <dimitrovo@gmail.com>
2025-03-26 23:36:34 +0200

    formatting

--- MagicTube.ino
+++ MagicTube.ino
@@ -1,8 +1,9 @@
+
 /* Arduino Nano Code for MagicElectronTube project MET3
- * http://
- * Copyright (c) 2023 Serhii Zaitsev <dimitrovo@gmail.com>
+ * https://github.com/dimitrovo/MagicTube
+ * Copyright (c) 2025 Serhii Zaitsev <dimitrovo@gmail.com>
  *
- * Using library: PinChangeInterrupt, FreqCount, SSD1306Ascii
+ * Using library:  FreqCount
  * Version 1.0
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -31,20 +32,12 @@
 #include <SPI.h>
 #include <EEPROM.h>
 
-
-
-
 //-------Begin hardware definition block---------------------------------------
-
 #define SWITCHER1 10
 #define SWITCHER2 11
 #define SWITCHER3 12
-int buttonState1 = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-//---------------------OLED----------------------------------------------------
-//#define I2C_ADDRESS 0x3C
-//SSD1306AsciiWire oled;
+
+
 //--------------------RDA5807 -------------------------------------------------
 #define RDA5807M_RANDOM_ACCESS_ADDRESS 0x11
 // Registers
@@ -53,10 +46,10 @@ int buttonState3 = 0;
 #define RDA5807M_REG_VOLUME 0x05
 #define RDA5807M_REG_RSSI   0x0B
 // FLAGS
-#define RDA5807M_FLG_DHIZ 0x8000 //15 DHIZ
-#define RDA5807M_FLG_DMUTE 0x4000  //14 DMUTE 
-#define RDA5807M_FLG_BASS 0x1000// 12 BASS
-#define RDA5807M_FLG_ENABLE word(0x0001)  //0  ENABLE
+#define RDA5807M_FLG_DHIZ 0x8000 
+#define RDA5807M_FLG_DMUTE 0x4000  
+#define RDA5807M_FLG_BASS 0x1000
+#define RDA5807M_FLG_ENABLE word(0x0001) 
 #define RDA5807M_FLG_TUNE word(0x0010)
 #define RDA5807M_FLG_MONO 0x2000 
 #define RDA5807M_BIT_MUTE 14
@@ -68,20 +61,11 @@ int buttonState3 = 0;
 #define RDA5807M_RSSI_MASK 0xFE00
 #define RDA5807M_RSSI_SHIFT 9
 //-----------------------AD9833----------------------------------------------
-//#define FNC_PIN 10 
 
-const byte numberOfDigits = 6; // number of digits in the frequense
-byte freqSGLo[numberOfDigits] = {0, 0, 0, 1, 0, 0}; // 1000Hz 
-byte freqSGHi[numberOfDigits] = {0, 0, 0, 0, 2, 0}; // 20kHz  
-
+const byte numberOfDigits = 6; 
+byte freqSGLo[numberOfDigits] = {0, 0, 0, 1, 0, 0}; 
 const int wSine     = 0b0000000000000000;
-//const int wTriangle = 0b0000000000000010;
-//const int wSquare   = 0b0000000000101000;
-
 int waveType = wSine;
-
-
-int SG_iSweep,SG_nSweep;
 
 
 const int SG_fsyncPin = 7;
@@ -90,127 +74,87 @@ const int SG_DATA = 9;
 
 long lOutFrequency = 0;
 //-------End hardware defenition block--------------------------------------------------
-
-
-
-
-
-
 //------------------------Global Frequencies defenitions ---------------
-#define FM_MIN 875 //FM radio range
+#define FM_MIN 875
 #define FM_MAX 1080
 
-const unsigned long c_MIN_OUT_FREQUENCY = 100000; //Low boundary of oscillator
-
-unsigned long g_lFrequency; // oscillating circuit frequency of tube  receiver
-const uint8_t g_FF=4; //frequency factor
-
-const unsigned long c_lMinPossibleFrq = 100000; //Max and Min possible generation range to restrict bad frequencies
+const unsigned long c_MIN_OUT_FREQUENCY = 100000; 
+unsigned long g_lFrequency; 
+const uint8_t g_FF=10; 
+const unsigned long c_lMinPossibleFrq = 100000;
 const unsigned long c_lMaxPossibleFrq = 1200000;
-
-unsigned long g_lMinFrq = 0; //Current range input frequency to map on FM range
+unsigned long g_lMinFrq = 0; 
 unsigned long g_lMaxFrq = 0;
-
-unsigned int g_iFMFreq; //FM frequency in Mhz*10
+unsigned int g_iFMFreq; 
 unsigned int g_iFMFreq_old = 1050;
 
-//Customizations store
 struct TCust{ 
   byte mode;
   unsigned long lMinFreq;
   unsigned long lMaxFreq;
-} cust, old_cust;
+} cust;
 
 bool bRangeChanged = false;
 const unsigned int cFixRange = 0;
 const unsigned int cDynRange = 1;
 unsigned int bMode = 0;
-bool bModeSaved = false;
 
-
-////Rest constant and variables
-const unsigned long threshold = 5000; // Поріг у 3 секунди
-unsigned long startTime;             // Час запуску Arduino
-//byte mode;                           // Змінна для роботи
-const int ledPin = 13;               // Вбудований LED на Arduino Nano
+const unsigned long threshold = 5000; 
+unsigned long startTime;             
+const int ledPin = 13;       
+        
 bool updated = false; 
 
 void setup() {
-  
+ 
 
+ pinMode(ledPin, OUTPUT); 
 
-// Serial.begin(115200);
-// Serial.println("Begin Setup");
- pinMode(ledPin, OUTPUT); // Налаштовуємо LED як вихід
-
- //Read and proceed saved customisation
-  EEPROM.get( 0, cust );
+ EEPROM.get( 0, cust );
   if(cust.lMinFreq < c_lMinPossibleFrq || cust.lMaxFreq > c_lMaxPossibleFrq ||  cust.mode < 0 || cust.mode > 1 ){
-   //  Serial.println("Can not read configuration");
      cust.lMinFreq = 0;
      cust.lMaxFreq = 0;
      cust.mode = cDynRange;
      bMode = cDynRange;
-  } //checkin read values for trash
+  } 
   EEPROM.put(0, cust);
 
-  //Serial.print("Mode on startup: ");
-  //Serial.println(cust.mode);
-
-  if (  cust.mode == cDynRange  ) { //no saved range, generate it in process
-   g_lMaxFrq = c_lMinPossibleFrq; //cross-initialize to fill data that are ready to compare operations (see loop)
+  
+ if (  cust.mode == cDynRange  ) { 
+   g_lMaxFrq = c_lMinPossibleFrq; 
    g_lMinFrq = c_lMaxPossibleFrq;
    bMode = cDynRange;
   } 
-  else { //mode Fix use loaded range
+  else { 
     g_lMaxFrq = cust.lMaxFreq;
     g_lMinFrq = cust.lMinFreq;
     bMode = cFixRange;
   }
 
-  // Встановлюємо mode = 1 на старті
-  //if we swith off the device less 3 sec, next mode will be DYNAMIC range.
+ 
    cust.mode = cDynRange;
    EEPROM.put(0, cust);
- //  Serial.println("Next start:Dynamics");
+   digitalWrite(ledPin, bMode == cDynRange ? HIGH : LOW);
 
-
-  // Запалюємо LED, якщо mode = 1
-  digitalWrite(ledPin, bMode == cDynRange ? HIGH : LOW);
-
-  // Запам'ятовуємо час початку роботи
-  startTime = millis();
+ 
+   startTime = millis();
   
-   //old_cust.lMaxFreq =0;
-   //old_cust.lMinFreq =0;
-   //EEPROM.put(0, old_cust); 
-   
-  
-
-  
- //Prepare hardware
   Wire.begin();
- // oled.begin(&Adafruit128x64, I2C_ADDRESS);
   FreqCount.begin(1000/g_FF);
   setupRDA5807();
   setRDA5807frequency(1036);
   setRDA5807volume(15); 
-
   pinMode(SWITCHER1,INPUT_PULLUP);
   pinMode(SWITCHER2,INPUT_PULLUP);
   pinMode(SWITCHER3,INPUT_PULLUP);
   
-
-  
-  //setAD9833frequency(465000);
-    InitSigGen();
     
-   
- //Serial.println("Started");
-
-   buttonState1 = digitalRead(SWITCHER1);
-   buttonState2 = digitalRead(SWITCHER2);
-   buttonState3 = digitalRead(SWITCHER3);
+  int buttonState1 = 0;
+  int buttonState2 = 0;
+  int buttonState3 = 0;
+  buttonState1 = digitalRead(SWITCHER1);
+  buttonState2 = digitalRead(SWITCHER2);
+  buttonState3 = digitalRead(SWITCHER3);
 
   if (buttonState1 == HIGH && buttonState2 == HIGH && buttonState3 == HIGH) lOutFrequency = 0;
   if (buttonState1 == LOW && buttonState2 == HIGH && buttonState3 == HIGH) lOutFrequency = 465000;
@@ -218,36 +162,22 @@ void setup() {
   if (buttonState1 == HIGH && buttonState2 == LOW && buttonState3 == HIGH) lOutFrequency = 460000;
   if (buttonState1 == HIGH && buttonState2 == LOW && buttonState3 == LOW) lOutFrequency = 470000;
   if (buttonState1 == HIGH && buttonState2 == HIGH && buttonState3 == LOW) lOutFrequency = 1000000;
-
- // Serial.print("Output Frequency: ");  Serial.println(lOutFrequency);
-    
-  SG_freqSet(lOutFrequency,wSine);
+  if (buttonState1 == LOW && buttonState2 == LOW && buttonState3 == LOW) lOutFrequency = 0;//Please enter your value
   
+  InitSigGen();
+  SG_freqSet(lOutFrequency,wSine);
 }
 
 
 
 void loop() {
 
-////Serial.println(millis());
-////delay(100);
-//
-// //if we swith off the device less 3 sec, next mode will be DYNAMIC range.
-// if(millis() > 3000 && bMode == cFixRange && ! bModeSaved) {
-//  EEPROM.put(0, cust); 
-//  bModeSaved = true;
-//  Serial.println("Next start:Fixed");
-// }
-// 
-
-  unsigned long elapsedTime = millis() - startTime;
+ unsigned long elapsedTime = millis() - startTime;
  digitalWrite(ledPin, bMode == cDynRange ? HIGH : LOW);
-   // Якщо пристрій пропрацював більше n секунд, оновлюємо mode у EEPROM на 0
   if (elapsedTime > threshold && !updated) {
     cust.mode = cFixRange;
     EEPROM.put(0, cust);
     updated = true;
-   // Serial.println("Device ran for more than 5 seconds. Next Mode set to Fix.");
   }
 
  if (FreqCount.available()) {
@@ -258,20 +188,16 @@ void loop() {
         if (g_lFrequency > c_lMinPossibleFrq && g_lFrequency < g_lMinFrq ) { g_lMinFrq = g_lFrequency; bRangeChanged = true; }
         if (g_lFrequency < c_lMaxPossibleFrq && g_lFrequency > g_lMaxFrq ) { g_lMaxFrq = g_lFrequency; bRangeChanged = true; }
     } 
-    g_iFMFreq = FM_MIN + (g_lFrequency - g_lMinFrq ) * (float(FM_MAX-FM_MIN)/float(g_lMaxFrq-g_lMinFrq));  //Set FM freq corresponds to circuit frequency
+    g_iFMFreq = FM_MIN + (g_lFrequency - g_lMinFrq ) * (float(FM_MAX-FM_MIN)/float(g_lMaxFrq-g_lMinFrq));  
     
-    if(g_iFMFreq != g_iFMFreq_old && g_lFrequency >= g_lMinFrq && g_lFrequency <= g_lMaxFrq  ) {  //to prevent parasit switching
+    if(g_iFMFreq != g_iFMFreq_old && g_lFrequency >= g_lMinFrq && g_lFrequency <= g_lMaxFrq  ) {  
       setRDA5807frequency(g_iFMFreq);
       g_iFMFreq_old = g_iFMFreq;
      } 
-
-    }  //0607
- //   Serial.print(millis()); Serial.print(" mode: ");Serial.print(bMode);
- //   Serial.print(" fr: ");Serial.print(g_lFrequency);Serial.print(" min:");Serial.print(g_lMinFrq);;Serial.print(" max:");Serial.print(g_lMaxFrq);
- //   Serial.print(" iFMF:");Serial.print(g_iFMFreq);Serial.println("=>");
+    }  
   }
 
- //Ranges changes
+ 
  if( bRangeChanged  && bMode == cDynRange ){
    bRangeChanged = false;  
    cust.lMinFreq = g_lMinFrq;
@@ -281,12 +207,6 @@ void loop() {
 }
 
 
-
-
-
-//-----------------------------------------------------------------------------
-//returns 10^y
-//-----------------------------------------------------------------------------
 unsigned long Power(int y) {
   unsigned long t = 1;
   for (byte i = 0; i < y; i++)
@@ -294,9 +214,6 @@ unsigned long Power(int y) {
   return t;
 }
 
-//-----------------------------------------------------------------------------
-//calculate the frequency from the array.
-//-----------------------------------------------------------------------------
 unsigned long calcFreq(byte* freqSG) {
   unsigned long i = 0;
   for (byte x = 0; x < numberOfDigits; x++)
@@ -304,9 +221,6 @@ unsigned long calcFreq(byte* freqSG) {
   return i;
 }
 
-//-----------------------------------------------------------------------------
-// SG_WriteRegister
-//-----------------------------------------------------------------------------
 void SG_WriteRegister(word dat) {
   digitalWrite(SG_CLK, LOW);
   digitalWrite(SG_CLK, HIGH);
@@ -325,19 +239,12 @@ void SG_WriteRegister(word dat) {
   digitalWrite(SG_fsyncPin, HIGH);
 }
 
-//-----------------------------------------------------------------------------
-// SG_Reset
-//-----------------------------------------------------------------------------
 void SG_Reset() {
   delay(100);
   SG_WriteRegister(0x100);
   delay(100);
 }
 
-//-----------------------------------------------------------------------------
-// SG_freqReset
-//    reset the SG regs then set the frequency and wave type
-//-----------------------------------------------------------------------------
 void SG_freqReset(long frequency, int wave) {
   long fl = frequency * (0x10000000 / 25000000.0);
   SG_WriteRegister(0x2100);
@@ -348,10 +255,6 @@ void SG_freqReset(long frequency, int wave) {
   waveType = wave;
 }
 
-//-----------------------------------------------------------------------------
-// SG_freqSet
-//    set the SG frequency regs 
-//-----------------------------------------------------------------------------
 void SG_freqSet(long frequency, int wave) {
   long fl = frequency * (0x10000000 / 25000000.0);
   SG_WriteRegister(0x2000 | wave);
@@ -360,9 +263,6 @@ void SG_freqSet(long frequency, int wave) {
 }
 
 void InitSigGen(void) {
- // pinMode(SG_POWER, OUTPUT);
- // digitalWrite(SG_POWER, HIGH);
-
   pinMode(SG_DATA, OUTPUT);
   pinMode(SG_CLK, OUTPUT);
   pinMode(SG_fsyncPin, OUTPUT);
@@ -372,8 +272,6 @@ void InitSigGen(void) {
   SG_freqReset(calcFreq(freqSGLo), waveType);
 }
 
-
-/////RDA5807
 
 void setRegister(uint8_t reg, const uint16_t value) {
   Wire.beginTransmission(0x11);
@@ -397,22 +295,18 @@ uint16_t getRegister(uint8_t reg) {
 void setupRDA5807()
 {
 uint16_t reg02h, reg03h, reg05h, reg0Bh;
-// Register 02h is settings
   reg02h = RDA5807M_FLG_ENABLE | RDA5807M_FLG_DHIZ | RDA5807M_FLG_DMUTE;
   setRegister(RDA5807M_REG_CONFIG, reg02h);
-  reg02h |= RDA5807M_FLG_BASS; //bass
+  reg02h |= RDA5807M_FLG_BASS; 
   setRegister(RDA5807M_REG_CONFIG, reg02h);
-  reg02h |= RDA5807M_FLG_MONO; //Mono mode
+  reg02h |= RDA5807M_FLG_MONO; 
   setRegister(RDA5807M_REG_CONFIG, reg02h);
 }
 
 void setRDA5807frequency(uint16_t freq)
 {
 uint16_t reg02h, reg03h, reg05h, reg0Bh;
-  // Register 03h is radio station selection
-  // After reset the default value in register 03h is 0
-  // So BAND = 00 (87..108MHz), STEP = 00 (100kHz).
-  reg03h = (freq - 870) << RDA5807M_CHAN_SHIFT; // chan = (freq - band) / space
+  reg03h = (freq - 870) << RDA5807M_CHAN_SHIFT; 
   setRegister(RDA5807M_REG_TUNING, reg03h | RDA5807M_FLG_TUNE);
 }
 
@@ -420,10 +314,8 @@ uint16_t reg02h, reg03h, reg05h, reg0Bh;
 void setRDA5807volume(uint8_t v)
 {
   uint16_t reg02h, reg03h, reg05h, reg0Bh;
-  // Register 05h. Set the volume, do not change the other bits
-  reg05h = getRegister(RDA5807M_REG_VOLUME); // read the current value
-  reg05h &= ~RDA5807M_VOLUME_MASK; // Reset the VOLUME bits
-  reg05h |= v << RDA5807M_VOLUME_SHIFT; // set a new volume
+  reg05h = getRegister(RDA5807M_REG_VOLUME); 
+  reg05h &= ~RDA5807M_VOLUME_MASK; 
+  reg05h |= v << RDA5807M_VOLUME_SHIFT; 
   setRegister(RDA5807M_REG_VOLUME, reg05h);
- 
 }
